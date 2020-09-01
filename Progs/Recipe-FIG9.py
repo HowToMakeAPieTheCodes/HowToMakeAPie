3ca9af15927f658ee4c2c342d16191344755070f
Elise Maigné <elise.maigne@inrae.fr>
2020-09-01 16:39:47 +0200

    correct fig9 program

--- Progs/Recipe-FIG9.py
+++ Progs/Recipe-FIG9.py
@@ -4,28 +4,29 @@ Description of the 'Gateau basque' pie recipe \n
 Great taste guaranteed!
 """
 
-Receipe= 'Gateau basque'
-NbPers=4
-Time=30 # minutes
+Recipe = 'Gateau basque'
+Time = 30  # minutes
 
-Ingr=['Egg', 'Sugar','Salt', 'Butter', 'Flour', 'Milk',
-      'Vanilla', 'Eggs', 'Rum']
-#Needed quantities for a 4 persons pie
-QtyFor4=['1' , '150', '1', '125', '230', '0.25', '2',
-         '2', '1'] # in grams
+Ingr = ['Egg', 'Sugar', 'Salt', 'Butter', 'Flour',
+        'Milk', 'Vanilla', 'Rum']
 
-Unit=['unit(s)','gr', 'pinch','gr', 'gr', 'L', 'pod',
-      'unit(s)', 'soup spoon' ]
+# Needed quantities for a 4 persons pie
+QtyFor4 = ['1', '150', '1', '125', '230',
+           '0.25', '2', '1'] 
+Unit = ['unit(s)', 'gr', 'pinch', 'gr', 'gr', 'L',
+        'pod', 'soup spoon']
 
-# Given a nb of persons, th function adapts
-# the required quantities    
-def adapt_qty(nb):
-    """Give the required quantity of each ingredient.
-    For a given number of people (nb parameter).
+def adapt_qty(nbpers):
+    """Return the quantity of each ingredient
+    for a given number of people.
     """
-    for i,j,k in zip(Ingr, QtyFor4, Unit) :
-        print('Ingredient', i, ':', float(j)*nb/4, k,
-              'required for a',nb, 'person pie' )
-    
-adapt_qty(4)    
+    for i, j, k in zip(Ingr, QtyFor4, Unit):
+        print('Ingredient', i, ':',
+              float(j) * nbpers / 4, k,
+              'required for a', nbpers, 'person pie')
+
+adapt_qty(4)
 adapt_qty(6)
+
+
+
